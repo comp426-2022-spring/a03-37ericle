2fd6842b548de9a0870d48aa9975ac02640d7a50
ericle <ericle@email.unc.edu>
2022-02-23 12:40:19 -0500

    added new endpoint

--- server.js
+++ server.js
@@ -6,16 +6,14 @@ const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%', port))
 })
 
-app.use(function(req, res){
-    res.status(404).send('404 NOT FOUND')
-});
 app.get('/app/', (req, res) => {
     // Respond with status 200
-        res.statusCode = 200;
+        res.status(200).end('OK')
+        res.type('text/plain')
     // Respond with status message "OK"
-        res.statusMessage = 'OK';
-        res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-        res.end(res.statusCode+ ' ' +res.statusMessage)
+        // res.statusMessage = 'OK';
+        // res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
+        // res.end(res.statusCode+ ' ' +res.statusMessage)
     });
 
 app.get('app/flip', (req, res) => {
@@ -25,3 +23,8 @@ app.get('app/flip', (req, res) => {
       }
     res.send(coinFlip())
 })
+
+app.use(function(req, res){
+    res.status(404).send('404 NOT FOUND')
+    res.type('text/plain')
+});
