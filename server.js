b434d126239c2c4cfa80baebb4145e901958513b
ericle <ericle@email.unc.edu>
2022-02-24 16:50:21 -0500

    edit flip endpoint

--- server.js
+++ server.js
@@ -6,22 +6,80 @@ const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%', port))
 })
 
+function coinFlip() {
+    return (Math.floor(Math.random() * 2) == 0) ? "heads" : "tails";
+  }
+
+function coinFlips(flips) {
+  var output = [];
+  for (let i = 0; i < flips; i++) {
+    output.push(coinFlip())
+  }
+  return output;
+}
+
+function countFlips(array) {
+    let countArray = [];
+    let headsCount = 0;
+    let tailsCount = 0;
+    var o = new Object();
+    array.forEach(function(item) {
+      if (item == "heads") {
+        headsCount += 1;
+      }
+      if (item == "tails") {
+        tailsCount += 1;
+      }
+    });
+    if (headsCount == 0) {
+      o = {tails: tailsCount}
+    }
+    else if (tailsCount == 0) {
+      o = {heads: headsCount}
+    }
+    else {
+      o = {heads: headsCount, tails: tailsCount}
+    }
+    return o;
+  }
+
+function flipACoin(call) {
+  let result = "";
+  let flip = (Math.floor(Math.random() * 2) == 0) ? "heads" : "tails";
+  if (flip == call) {
+    result = "win"
+  }
+  else {
+    result = "lose"
+  }
+  let o = {call: call, flip: flip, result: result}
+  return o;
+}  
+
+function flipACoin(call) {
+    let result = "";
+    let flip = (Math.floor(Math.random() * 2) == 0) ? "heads" : "tails";
+    if (flip == call) {
+      result = "win"
+    }
+    else {
+      result = "lose"
+    }
+    let o = {call: call, flip: flip, result: result}
+    return o;
+}
+
 app.get('/app/', (req, res) => {
         res.status(200).end('OK')
         res.type('text/plain')
     });
 
-app.get('/app/echo/:number', (req, res) => {
-    res.status(200).json({'message': req.params.number})
+app.get('/app/flip', (req, res) => {
+    res.status(200)
+    res.type('text/plain')
+    res.json({'flip': coinFlip()})
 })
 
-app.get('/app/flip', (req, res) => {
-    res.type('text/plain')
-    function coinFlip() {
-        return (Math.floor(Math.random() * 2) == 0) ? "heads" : "tails";
-      }
-    res.send(coinFlip())
-})
 
 app.use(function(req, res){
     res.status(404).send('404 NOT FOUND')
